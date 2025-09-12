6b6aa5be6c04dbf809c8b6fec91d51704ecf694d
SanzSeraph <snoop.daniels@gmail.com>
2025-09-12 13:08:36 -0400

    Adding more parsing logic

--- JackCompiler/parse_xml.c
+++ JackCompiler/parse_xml.c
@@ -6,6 +6,8 @@
 #include <stdint.h>
 #define MAX_NAMESPACE_LENGTH 128
 #define MAX_ELEMENT_NAME_LENGTH 1024
+#define MAX_ATTRIBUTE_NAME_LENGTH 256
+#define MAX_ATTRIBUTE_VALUE_LENGTH 2048
 
 bool isLegalElementFirstCharacter(char c);
 bool isLegalElementSubsequentCharacter(char c);
@@ -13,7 +15,8 @@ bool isWhitespace(char c);
 
 struct ParseResult parseXmlElement(FILE *file, size_t currentLine, size_t currentColumn);
 struct ParseResult parseAttributes(FILE* file, size_t currentLine, size_t currentColumn);
-struct ParseResult consumeWhitespace(FILE* file, size_t* currentLine, size_t* currentColumn);
+struct ParseResult consumeWhitespace(FILE* file, size_t currentLine, size_t currentColumn);
+struct ParseResult consumeProlog(FILE* file, size_t currentLine, size_t currentColumn);
 
 struct ParseResult parseXml(char* path) 
 {
@@ -36,12 +39,31 @@ struct ParseResult parseXml(char* path)
     size_t currentLine = 1;
 	size_t currentColumn = 0;
     
+	consumeWhitespace(file, &currentLine, &currentColumn);
+
     char currentChar;
     struct KeyValueCollection* namespaces = KeyValueCollection_new();
 
     while ((currentChar = fgetc(file)) != EOF) {
         if (currentChar == '<') {
             currentColumn++;
+
+            currentChar = fgetc(file);
+			currentColumn++;
+
+            if (currentChar == '?') {
+                struct ParseResult prologParseResult = consumeProlog(file, &currentLine, &currentColumn);
+
+                if (prologParseResult.code != PARSE_XML_SUCCESS) {
+                    result.code = prologParseResult.code;
+
+                    goto ret;
+				}
+            } else {
+                ungetc(currentChar, file);
+				currentColumn--;
+            }
+
             struct ParseResult rootNodeParseResult = parseXmlElement(file, currentLine, currentColumn, namespaces);
 
             size_t count = DynamicArray_count(((struct Node *)result.value)->children);
@@ -67,6 +89,14 @@ ret:
     return result;
 }
 
+/// <summary>
+/// Assumes that the < has already been consumed
+/// </summary>
+/// <param name="file"></param>
+/// <param name="currentLine"></param>
+/// <param name="currentColumn"></param>
+/// <param name="namespaces"></param>
+/// <returns></returns>
 struct ParseResult parseXmlElement(FILE *file, size_t currentLine, size_t currentColumn, struct KeyValueCollection *namespaces) 
 {
     struct ParseResult result = (struct ParseResult){
@@ -74,13 +104,13 @@ struct ParseResult parseXmlElement(FILE *file, size_t currentLine, size_t curren
         .value = NULL
     };
 
-    char c = fgetc(file);
+    char currentChar = fgetc(file);
     
-    if (!isLegalElementFirstCharacter(c)) {
-        ungetc(c, file);
+    if (!isLegalElementFirstCharacter(currentChar)) {
+        ungetc(currentChar, file);
 
         result.code = PARSE_XML_INVALID_TAG_CHARACTER;
-        sprintf(result.message, "Illegal first character %c at line %lu column %lu", c, currentLine, currentColumn);
+        sprintf(result.message, "Illegal first character %c at line %lu column %lu", currentChar, currentLine, currentColumn);
         result.line = currentLine;
         result.column = currentColumn;
     
@@ -107,7 +137,7 @@ struct ParseResult parseXmlElement(FILE *file, size_t currentLine, size_t curren
 
     short index = 0;
 
-    while ((c = fgetc(file)) != EOF && !isWhitespace(c)) {
+    while ((currentChar = fgetc(file)) != EOF && !isWhitespace(currentChar)) {
         currentColumn++;
 
         if (index >= MAX_ELEMENT_NAME_LENGTH) {
@@ -119,36 +149,38 @@ struct ParseResult parseXmlElement(FILE *file, size_t currentLine, size_t curren
             goto ret;
         }
 
-        if (!isLegalElementSubsequentCharacter(c)) {
+        if (!isLegalElementSubsequentCharacter(currentChar)) {
             result.code = PARSE_XML_INVALID_TAG_CHARACTER;
-            sprintf(result.message, "Illegal character %c at line %lu column %lu", c, currentLine, currentColumn);
+            sprintf(result.message, "Illegal character %c at line %lu column %lu", currentChar, currentLine, currentColumn);
             result.line = currentLine;
             result.column = currentColumn;
 
             goto ret;
         }
 
-        if (c == ':') {
+        if (currentChar == ':') {
             memcpy(namespace, elementName, sizeof(namespace));
             memset(elementName, 0, sizeof(elementName));
         } else {
-            elementName[index] = c;
+            elementName[index] = currentChar;
         }
     }
 
-    if (c == EOF) {
+    if (currentChar == EOF) {
         result.code = PARSE_XML_UNEXPECTED_EOF;
         sprintf(result.message, "Unexpected end of file while parsing element %s:%s", namespace, elementName);
         result.column = currentColumn;
         result.line = currentLine;
 
         goto ret;
-    } if (c == ' ' || c == '\t') {
-        currentColumn++;
-    } else if (c == '\n') {
+    } 
+    
+    if (currentChar == '\n') {
         currentColumn = 1;
         currentLine++;
     }
+
+	struct ParseResult consumeWhitespaceResult = consumeWhitespace(file, currentLine, currentColumn);
 
     result.value = ElementNode_new(namespace, elementName);
 
@@ -157,8 +189,11 @@ struct ParseResult parseXmlElement(FILE *file, size_t currentLine, size_t curren
     struct ParseResult parseAttributesResult = parseAttributes(file, &currentLine, &currentColumn);
 
     if (parseAttributesResult.code != PARSE_XML_SUCCESS) {
-        result = parseAttributesResult;
-
+        result.code == parseAttributesResult.code;
+        result.line = parseAttributesResult.line;
+		result.column = parseAttributesResult.column;
+        sprintf(result.message, parseAttributesResult.message);
+		
         goto ret;
     }
 
@@ -183,15 +218,153 @@ struct ParseResult parseAttributes(FILE* file, size_t *currentLine, size_t *curr
         goto ret;
     }
 
-    char c;
+    char currentChar;
+    char key[MAX_ATTRIBUTE_NAME_LENGTH];
+	char value[MAX_ATTRIBUTE_NAME_LENGTH];
+
+    while ((currentChar = fgetc(file))) {
+
+    }
 
     ret: 
     return result;
 }
 
-struct ParseResult consumeWhitespace(FILE *file, size_t *currentLine, size_t *currentColumn) 
+struct ParseResult consumeWhitespace(FILE *file, size_t currentLine, size_t currentColumn) 
 {
+    struct ParseResult result = {
+        .code = PARSE_XML_SUCCESS,
+        .value = NULL
+	};
+
+    char currentChar;
+
+    while((currentChar = fgetc(file)) != EOF && (currentChar == ' ' || currentChar == '\t' || currentChar == '\n')) {
+        if (currentChar == '\n') {
+            currentLine++;
+            currentColumn = 0;
+        } else {
+            currentColumn++;
+        }
+	}
+
+    if (currentChar == EOF) {
+		result.code = PARSE_XML_UNEXPECTED_EOF;
+        result.line = currentLine;
+		result.column = currentColumn;
+        sprintf(result.message, "Unexpected end of file");
+        
+        goto ret;
+    }
+
+	ungetc(currentChar, file);
+    currentColumn--;
+    result.line = currentLine;
+	result.column = currentColumn;
     
+    ret:
+        return result;
+}
+
+/// <summary>
+/// Assumes that the <? part of the prolog have already been consumed
+/// </summary>
+struct ParseResult consumeProlog(FILE* file, size_t currentLine, size_t currentColumn) 
+{
+    struct ParseResult result = {
+        .code = PARSE_XML_SUCCESS,
+        .value = NULL
+	};
+
+    char currentChar = fgetc(file);
+    currentColumn++;
+
+    if (currentChar == EOF) {
+		result.code = PARSE_XML_UNEXPECTED_EOF;
+		result.line = currentLine;
+		result.column = currentColumn;
+
+        goto ret;
+    }
+	
+    if (currentChar != 'x') {
+		result.code = PARSE_XML_INVALID_TAG_CHARACTER;
+        result.line = currentLine;
+		result.column = currentColumn;
+		sprintf(result.message, "Expected 'xml' at line %lu column %lu", currentLine, currentColumn);
+
+        goto ret;
+    }
+
+	currentChar = fgetc(file);
+	currentColumn++;
+
+    if (currentChar != 'm') {
+        result.code = PARSE_XML_INVALID_TAG_CHARACTER;
+		result.line = currentLine;
+        result.column = currentColumn;
+		sprintf(result.message, "Expected 'xml' at line %lu column %lu", currentLine, currentColumn);
+
+		goto ret;
+    }
+
+	currentChar = fgetc(file);
+    currentColumn++;
+
+    if (currentChar != 'l') {
+        result.code = PARSE_XML_INVALID_TAG_CHARACTER;
+		result.line = currentLine;
+		result.column = currentColumn;
+		sprintf(result.message, "Expected 'xml' at line %lu column %lu", currentLine, currentColumn);
+
+        goto ret;
+    }
+
+	currentChar = fgetc(file);
+	currentColumn++;
+
+    if (currentChar != ' ') {
+        result.code = PARSE_XML_INVALID_TAG_CHARACTER;
+		result.line = currentLine;
+		result.column = currentColumn;
+		sprintf(result.message, "Expected whitespace after 'xml' at line %lu column %lu", currentLine, currentColumn);
+
+        goto ret;
+    }
+
+    bool inString = false;
+    
+    while ((currentChar = fgetc(file)) != EOF) {
+        if (currentChar = '\n') {
+            currentLine++;
+            currentColumn = 0;
+        }
+        else {
+            currentColumn++;
+        }
+        		
+        if (currentChar == '"') {
+            inString = !inString;
+        }
+        else if (currentChar == '?' && !inString) {
+            currentChar = fgetc(file);
+            currentColumn++;
+            if (currentChar != '>') {
+                result.code = PARSE_XML_INVALID_TAG_CHARACTER;
+                result.line = currentLine;
+                result.column = currentColumn;
+                sprintf(result.message, "Expected > to close the prolog at line %lu column %lu", currentLine, currentColumn);
+
+                goto ret;
+            }
+            else {
+                goto ret;
+            }
+        }
+    }
+
+    ret:
+	return result;
 }
 
 bool isLegalElementFirstCharacter(char c)
